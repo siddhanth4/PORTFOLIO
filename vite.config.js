bc53ba1d9efe4417e49809c5698d973cbb7b8036
siddhanth4 <123siddhanth@gmail.com>
2025-01-20 23:41:31 +0530

    modified PORT

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Fallback to 3000 if PORT is not set
+  },
 })
