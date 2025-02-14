68361d047d44322d19194b761b7e790584b5a09a
siddhanth4 <123siddhanth@gmail.com>
2025-02-14 12:53:09 +0530

    updated scale

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,7 +20,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.25 : 0.8} // Further reduced scale for mobile
+        scale={isMobile ? 0.25 : 0.7} // Further reduced scale for mobile
         position={isMobile ? [0, -1, -1] : [0, -3.25, -1.5]} // Adjusted position for mobile
         rotation={[-0.01, -0.2, -0.1]}
       />
